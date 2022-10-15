57723900dbd530a0f6c0fed2cf21f22c21308864
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-10-14 22:03:30 -0300

    finish project with Redux Saga

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
-import { useDispatch } from "react-redux";
-import { addProductToCart } from "../store/modules/cart/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { IState } from "../store";
+import { addProductToCartRequest } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
 
 interface CatalogItemProps {
@@ -10,8 +11,12 @@ interface CatalogItemProps {
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
 
+  const hasFailedStockCheck = useSelector<IState, boolean>((state) =>
+    state.cart.failedStockCheck.includes(product.id)
+  );
+
   const handleAddProductToCard = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
 
   return (
@@ -20,6 +25,9 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
       <button type="button" onClick={handleAddProductToCard}>
         Comprar
       </button>
+      {hasFailedStockCheck && (
+        <span style={{ color: "red" }}>Falta de estoque</span>
+      )}
     </article>
   );
 };
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,15 +1,16 @@
 import { Reducer } from "redux";
 import produce from "immer";
-import { ICartState } from "./types";
+import { ActionTypes, ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
   items: [],
+  failedStockCheck: [],
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
-      case "ADD_PRODUCT_TO_CART": {
+      case ActionTypes.addProductToCartSuccess: {
         const { product } = action.payload;
 
         const productInCartIndex = draft.items.findIndex(
@@ -18,6 +19,12 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
 
         if (productInCartIndex >= 0) draft.items[productInCartIndex].quantity++;
         else draft.items.push({ product, quantity: 1 });
+
+        break;
+      }
+
+      case ActionTypes.addProductToCartFailure: {
+        draft.failedStockCheck.push(action.payload.productId);
 
         break;
       }
